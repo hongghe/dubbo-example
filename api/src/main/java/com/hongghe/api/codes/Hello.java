0ebaa8bf201c7a7175556a3dbdedf726896aef03
hongghe <364930855@qq.com>
2018-07-08 16:49:57 +0800

    properties of redis.

---
+++ api/src/main/java/com/hongghe/api/codes/Hello.java
@@ -0,0 +1,11 @@
+package com.hongghe.api.codes;
+
+/**
+ * The hello world program
+ *
+ * @author hongghe
+ */
+public interface Hello {
+    public String sayHello();
+    public String setGreetMessage();
+}
---
+++ api/src/main/java/com/hongghe/api/utility/TimeUtility.java
@@ -0,0 +1,13 @@
+package com.hongghe.api.utility;
+
+/**
+ * The time what is time compute methods and tools.
+ *
+ * @author hongghe
+ */
+public class TimeUtility {
+
+    public static Long getTimestamp() {
+        return System.currentTimeMillis();
+    }
+}
