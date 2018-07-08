010a79f1bfdffcbfe7fdde922a514b02d0efc78a
hongghe <364930855@qq.com>
2018-07-08 15:26:25 +0800

    Initial commit

---
+++ dubbo-rpc/src/main/java/com/hongghe/dubborpc/DubboRpcApplication.java
@@ -0,0 +1,17 @@
+package com.hongghe.dubborpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The start class of project.
+ *
+ * @author hongghe
+ */
+@SpringBootApplication
+public class DubboRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboRpcApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hongghe/dubboexample/DubboExampleApplication.java
@@ -0,0 +1,17 @@
+package com.hongghe.dubboexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The start class of application
+ *
+ * @author hongghe
+ */
+@SpringBootApplication
+public class DubboExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboExampleApplication.class, args);
+	}
+}
