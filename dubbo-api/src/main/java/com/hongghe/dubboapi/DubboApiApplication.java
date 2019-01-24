559e2846a689c94cc58670f6390e5dcdb7190688
hongghe <364930855@qq.com>
2019-01-24 20:52:13 +0800

    message

--- dubbo-api/src/main/java/com/hongghe/dubboapi/DubboApiApplication.java
+++ dubbo-api/src/main/java/com/hongghe/dubboapi/DubboApiApplication.java
@@ -2,12 +2,16 @@ package com.hongghe.dubboapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * The startup class of project.
  *
  * @author hongghe 12/08/2018
  */
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class DubboApiApplication {
 
